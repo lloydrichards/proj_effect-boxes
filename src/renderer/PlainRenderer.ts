e3e3fc5dac2a7ed3b12c1fd4eadebaed19439af5
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-10-02 19:41:32 +0200

    refactor: move renderers to own dir

---
+++ src/renderer/PlainRenderer.ts
@@ -0,0 +1,42 @@
+import { Effect, Layer } from "effect";
+import type * as Annotation from "../Annotation";
+import type * as Box from "../Box";
+import type * as R from "../Renderer";
+import { takeP, takePA } from "../internal/box";
+import { Renderer, renderBox } from "../internal/renderer";
+import * as Width from "../internal/width";
+
+/** @internal */
+
+export const makePlainRenderer = Layer.effect(
+  Renderer,
+  Effect.gen(function* () {
+    const processor: R.TextProcessor = {
+      processLine: (text: string, targetWidth: number) =>
+        takeP(Width.segments(text), " ", targetWidth).join(""),
+      processLineAligned: (
+        text: string,
+        targetWidth: number,
+        alignment: Box.Alignment
+      ) => takePA(Width.segments(text), alignment, " ", targetWidth).join(""),
+      preservesFormatting: false,
+    };
+
+    const postProcess = <A>(
+      lines: string[],
+      _annotation?: Annotation.Annotation<A>
+    ) => Effect.succeed(lines);
+
+    const renderContent = <A>(box: Box.Box<A>): Effect.Effect<string[]> =>
+      Effect.gen(function* () {
+        const lines = yield* renderBox(box, processor, renderContent);
+        return yield* postProcess(lines, box.annotation);
+      });
+
+    return {
+      processor,
+      renderContent,
+      postProcess,
+    };
+  })
+);
