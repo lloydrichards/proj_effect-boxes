7038354e436f67bf28fd8c7253bcf6011c9b753e
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-05-25 14:39:04 +0200

    docs: document for all the guides

--- apps/docs/app/nav.config.ts
+++ apps/docs/app/nav.config.ts
@@ -20,6 +20,7 @@ export const navigation: NavSection[] = [
       { label: "Using Annotation", href: "/guides/using-annotation" },
       { label: "Using Ansi", href: "/guides/using-ansi" },
       { label: "Using Layout", href: "/guides/using-layout" },
+      { label: "Using Cmd", href: "/guides/using-cmd" },
       { label: "Rendering", href: "/guides/rendering" },
       { label: "Common Patterns", href: "/guides/common-patterns" },
     ],
